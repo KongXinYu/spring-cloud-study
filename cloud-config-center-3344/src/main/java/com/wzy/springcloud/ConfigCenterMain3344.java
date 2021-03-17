8fdcb02722a9ca0bbb322067163128d0810504d9
Wzy <123@qq.com>
2021-03-17 16:39:29 +0800

    1.新增spring cloud config demo

---
+++ cloud-config-center-3344/src/main/java/com/wzy/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.wzy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: WuZY
+ * @time: 2021/3/16 0016
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/wzy/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.wzy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: WuZY
+ * @time: 2021/3/17 0017
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
