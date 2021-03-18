8b93589c2123ea4cc64a06168aad329c9d9c1def
Wzy <123@qq.com>
2021-03-18 13:12:58 +0800

    1.新增spring cloud stream 相关

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/wzy/springcloud/service/IMessageProvider.java
@@ -0,0 +1,11 @@
+package com.wzy.springcloud.service;
+
+/**
+ * @description:
+ * @author: WuZY
+ * @time: 2021/3/18 0018
+ */
+public interface IMessageProvider {
+
+    public String send();
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/wzy/springcloud/service/impl/MessageProviderImpl.java
@@ -0,0 +1,30 @@
+package com.wzy.springcloud.service.impl;
+
+import com.wzy.springcloud.service.IMessageProvider;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+/**
+ * @description:
+ * @author: WuZY
+ * @time: 2021/3/18 0018
+ */
+@EnableBinding(Source.class)
+public class MessageProviderImpl implements IMessageProvider {
+
+    @Resource
+    private MessageChannel output; // 消息发送管道
+
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(serial).build());
+        System.out.println("*****serial:" + serial);
+        return null;
+    }
+}
